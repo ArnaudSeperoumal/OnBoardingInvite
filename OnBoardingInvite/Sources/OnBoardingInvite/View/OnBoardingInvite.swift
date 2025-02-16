76a98fecbc8c892e9c1beb5a2836598fca55031f
Arnaud SEPEROUMAL <arnaudseperoumal@gmail.com>
2025-02-16 19:04:12 +0100

    update animation

--- OnBoardingInvite/Sources/OnBoardingInvite/View/OnBoardingInvite.swift
+++ OnBoardingInvite/Sources/OnBoardingInvite/View/OnBoardingInvite.swift
@@ -3,41 +3,74 @@ import SwiftUI
 let delta: CGFloat = 1.2
 public typealias ViewIdentifiable = View & Identifiable & Equatable
 
-public struct OnBoardingInviteView<Content> : View where Content : ViewIdentifiable {
-//public struct OnBoardingInviteView: View {
+public struct OnBoardingInviteView<Content, Content2> : View where Content : ViewIdentifiable, Content2 : View {
     var cards: [Content]
+    var subview: Content2
+    var spacing: CGFloat
+    var relativeFrame: CGFloat
+    var cardWidth: CGFloat
     @State private var activeCard: Content
+    @State private var scrollPosition: ScrollPosition = .init()
+    @State private var currentScrollOffset: CGFloat = 0
+    @State private var timer = Timer.publish(every: 0.01, on: .current, in: .default).autoconnect()
     
-    public init(cards: [Content]) {
+    public init(cards: [Content], subview: Content2, spacing: CGFloat = 35, relativeFrame: CGFloat = 0.55, cardWidth: CGFloat = 0) {
         guard let first = cards.first else {
             fatalError("No cards")
         }
         self.cards = cards
-        self.activeCard = first // as! ObjectIdentifier
+        self.activeCard = first
+        self.subview = subview
+        self.spacing = spacing
+        self.relativeFrame = relativeFrame
+        self.cardWidth = cardWidth
     }
 
     public var body: some View {
         ZStack {
             AmbiantBck()
+                .animation(.easeInOut(duration: 1))
             
-            VStack(spacing: 35) {
+            VStack(spacing: spacing) {
                 InfiniteScrollView {
                     ForEach(cards) { card in
-//                            CardView(card: card)
-                        card
-                            .scrollTransition(.interactive.threshold(.centered), axis: .horizontal) { content, phase in
-                                        content
-                                            .offset(y: phase == .identity ? -10 : 0)
-                                            .rotationEffect(.degrees(phase.value * 5), anchor: .bottom)
-                                    }
+                        GeometryReader { proxy in
+                            let _ = print("proxy.size: \(proxy.size)")
+                            card
+                                .scrollTransition(.interactive.threshold(.centered), axis: .horizontal) { content, phase in
+                                    content
+                                        .offset(y: phase == .identity ? -10 : 0)
+                                        .rotationEffect(.degrees(phase.value * 5), anchor: .bottom)
+                                }
+                        }
                     }
                 }
                 .scrollIndicators(.hidden)
+                .scrollPosition($scrollPosition)
                 .containerRelativeFrame(.vertical) { value, _ in
-                    value * 0.65
+                    value * self.relativeFrame
                 }
+                .onScrollGeometryChange(for: CGFloat.self) { scrollGeometry in
+                    let retour = scrollGeometry.contentOffset.x + scrollGeometry.contentInsets.leading
+                    return retour
+                } action: { oldValue, newValue in
+                    currentScrollOffset = newValue
+                    if cardWidth != 0 {
+                        let activeIndex = Int((currentScrollOffset / cardWidth).rounded()) % cards.count
+                        if activeCard != cards[activeIndex] {
+                            activeCard = cards[activeIndex]
+                            let _ = print("activeCard: \(activeCard)")
+                        }
+                    }
+                }
+
+                subview
             }
             .safeAreaPadding(15)
+        }
+        .onReceive(timer) { _ in
+            currentScrollOffset += 0.3
+            scrollPosition.scrollTo(x: currentScrollOffset)
         }
     }
     
@@ -47,20 +80,13 @@ public struct OnBoardingInviteView<Content> : View where Content : ViewIdentifia
             let size = $0.size
             let width = size.width * delta
             let height = size.height * delta
-        let _ = print("size: \(size)")
             ZStack {
                 ForEach(cards) { card in
-//////                    Image(card.image)
-//////                        .resizable()
-//////                        .aspectRatio(contentMode: .fill)
-//                    if activeCard == card {
-                        
                         card
                             .ignoresSafeArea()
                             .frame(width: width, height: height)
                             .offset(x: -(width - size.width) / 2, y: -(height - size.height) / 2)
                             .opacity(activeCard == card ? 1 : 0)
-//                    }
                 }
 
                 Rectangle()
@@ -72,23 +98,4 @@ public struct OnBoardingInviteView<Content> : View where Content : ViewIdentifia
             .ignoresSafeArea()
         }
     }
-    
-//    @ViewBuilder
-//    private func CardView(card: Card) -> some View {
-//        GeometryReader {
-//            let size = $0.size
-//            
-//            Image(card.image)
-//                .resizable()
-//                .aspectRatio(contentMode: .fit)
-//                .frame(width: size.width, height: size.height)
-//                .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 1)
-//        }
-//        .frame(width: 220)
-//        .scrollTransition(.interactive.threshold(.centered), axis: .horizontal) { content, phase in
-//            content
-//                .offset(y: phase == .identity ? -10 : 0)
-//                .rotationEffect(.degrees(phase.value * 5), anchor: .bottom)
-//        }
-//    }
 }
--- OnBoardingInviteDemo/OnBoardingInviteDemo/ContentView.swift
+++ OnBoardingInviteDemo/OnBoardingInviteDemo/ContentView.swift
@@ -2,13 +2,31 @@ import SwiftUI
 import OnBoardingInvite
 
 struct ContentView: View {
+    var subview: some View {
+        VStack(spacing: 10) {
+            Text("Welcome to")
+                .font(.headline)
+                .foregroundStyle(.gray)
+            
+            Text("OnBoarding Invite")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+            
+            Text("Create beautiful onboarding screens with ease with OnBoarding Invite Package. Anyone can use it, from beginners to experts.\n it's customizable and easy to use.")
+                .font(.subheadline)
+                .foregroundStyle(.gray)
+                .multilineTextAlignment(.center)
+        }
+    }
+    
     var body: some View {
         OnBoardingInviteView(cards: [
             Carte("card1"),
             Carte("card2"),
             Carte("card3"),
             Carte("card4")
-        ])
+        ], subview: subview)
     }
 }
 
